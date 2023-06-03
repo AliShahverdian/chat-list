325c3d038dce9fc47f1d13e097f91929a540bfb7
AliShahverdian <ali.shahverdian@gmail.com>
2023-06-04 00:24:22 +0100

    creating profile page

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,21 +1,61 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-col>
-  </v-row>
+  <div>
+    <v-card v-if="showCard" class="mx-auto" max-width="400">
+      <v-img
+        class="align -end text-white"
+        height="200"
+        :src="user.avatar"
+        cover
+      >
+      </v-img>
+
+      <v-card-text class="text-center">
+        <div>{{ user.name }}</div>
+        <div>{{ user.email }}</div>
+        <div>{{ user.mobile }}</div>
+      </v-card-text>
+
+      <v-card-actions class="d-flex justify-center">
+        <v-btn color="orange" @click="editProfile">Edit </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-form v-else @submit.prevent="editForm">
+      <v-text-field v-model="user.name" outlined />
+      <v-text-field v-model="user.email" outlined />
+      <v-text-field v-model="user.mobile" outlined />
+      <v-btn type="submit">submit</v-btn>
+    </v-form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'InspirePage',
+  data() {
+    return {
+      user: {},
+      showCard: true,
+    }
+  },
+  created() {
+    if (process.browser) {
+      const userProfile = localStorage.getItem('loginUser')
+      this.user = JSON.parse(userProfile)
+    }
+  },
+  methods: {
+    editProfile() {
+      this.showCard = false
+    },
+    async editForm() {
+      try {
+        await this.$axios.put(
+          `http://localhost:3000/users/${this.user.id}`,
+          this.user
+        )
+      } catch (err) {
+        console.log(err)
+      }
+    },
+  },
 }
 </script>
